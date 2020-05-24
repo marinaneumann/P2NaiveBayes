c2d5554c6207b257e77e27263b201ca0cf615a74
Marina Neumann <marinaneumann2468@yahoo.com>
2020-05-23 21:48:32 -0700

    Cleanup of unncessary code

--- P2NB.py
+++ P2NB.py
@@ -30,24 +30,9 @@ class spamClassifer():
         self.size = 58
         self.numTrain = len(train)
         self.numTest = len(test)
-        #Below is used solely for the means of using the algorithm on test data
-        # arrays1 = []
-        # arrays0 = []
-        # for i in self.test:
-        #     if i[57] == 1:
-        #         arrays1.append(i)
-        #     else:
-        #         arrays0.append(i)
-        # self.test0 = arrays0
-        # self.test1 = arrays1
-        # print( "WTF?",arrays0[0][57])
+
     #Sets up probablistic model to gather prior probabilities, mean and std. dev.
     def model(self):
-        #Variables below used to distinguish between separate means for whether 0 or 1 class.
-        means0 = []
-        stDevs0 = []
-        means1 = []
-        stDevs1 = []
         #Used for splitting of self.train into arrays of class 0 and arrays of class 1
         arrays0 =[]
         arrays1 =[]
@@ -62,20 +47,8 @@ class spamClassifer():
             else:
                 arrays0.append(i)
                 countNot +=1
-        #Used to take mean/std deviation of all arrays with class 0
-        for column in zip(*arrays0):
-            means0.append(mean(column))
-            stDevs0.append(stdev(column))
-        #Below deletes means and std deviations of 58th element as that takes mean/std dev of class labels (0 or 1)
-        del(means0[-1])
-        del(stDevs0[-1])
-        # Used to take mean/std deviation of all arrays with class 1
-        for column in zip(*arrays1):
-            means1.append(mean(column))
-            stDevs1.append(stdev(column))
-        #Deletes mean/std dev of clas labels as unnecessary
-        del (means1[-1])
-        del (stDevs1[-1])
+        means0, stDevs0, means1, stDevs1 = arrayStatistics(arrays0, arrays1)
+
         #Computes prior probability of spam vs not spam
         probSpam = countSpam/ self.numTrain
         probNotSpam = countNot/self.numTrain
@@ -101,47 +74,49 @@ class spamClassifer():
             else:
                 arrays0.append(i)
                 countNot +=1
-        means0 = []
-        stDevs0 = []
-        means1 = []
-        stDevs1 = []
 
         probabilities = []
         probabilities0 = []
         probabilities1 = []
         finalprob0 = [] #Will hold all class 0(x) probabilities
         finalprob1 = [] #Will hold all class 1(x) probabilities
-        for column in zip(*arrays0):
-            means0.append(mean(column))
-            stDevs0.append(stdev(column))
-        del (means0[-1])
-        del (stDevs0[-1])
+
+        means0, stDevs0, means1, stDevs1 = arrayStatistics(arrays0, arrays1)
         cNum = 1 #cNums are used as indicators for testClassProb function, to specify what is to be returned later
+        #pdf = calculateProb(arrays0[0], means0,stDevs0)
+        #print("Pdf of arrays0:",pdf)
 
+        probs = 1
         for i in arrays0:
-            for z in range(0,57):
-                probs = calculateProb(i[z], means0[z], stDevs0[z])
-                probabilities.append(probs)
-            probabilities0.append(np.log(probabilities))
-        print(probabilities0)
-        probNum0 = countNot / self.numTest
-        #finalprob0.append(np.argmax(sum(probabilities0, np.log(probNum0))))
-        # #print("Final probabilities of class 0:", finalprob0)
+           # for z in range(0,57):
+            probs *= calculateProb(i, means0, stDevs0)
+            #probabilities.append(probs)
+            #print("Conditional probability of",i, "is:", probs)
+            #probabilities0.append(np.log(probs))
+        print("The probability of class 0 is:", probs)
+        #print(probabilities0)
+        #print(len(arrays0))
+        # probNum0 = countNot / self.numTest
+        # probNum0 = np.log(probNum0)
+        # goMan = sum(probabilities0, probNum0)
+        # finalprob0 = np.argmax(goMan)
+        #finalprob0 = np.argmax(sum(probabilities0, np.log(probNum0)))
+        #print("Final probabilities of class 0:", finalprob0)
 
-        probabilities.clear()
-        for column in zip(*arrays1):
-            means1.append(mean(column))
-            stDevs1.append(stdev(column))
-        del (means1[-1])
-        del (stDevs1[-1])
-        cNum =0
-        for m in arrays1:
-            for z in range(0,57):
-                probs = calculateProb(m[z], means1[z], stDevs1[z])
-                #probabilities.append(probs)
-            probabilities1.append(np.log(probabilities))
-        print(probabilities1)
-        probNum1 = countSpam/self.numTest
+        # probabilities.clear()
+        # for column in zip(*arrays1):
+        #     means1.append(mean(column))
+        #     stDevs1.append(stdev(column))
+        # del (means1[-1])
+        # del (stDevs1[-1])
+        # cNum =0
+        # for m in arrays1:
+        #     for z in range(0,57):
+        #         probs = calculateProb(m[z], means1[z], stDevs1[z])
+        #         probabilities.append(probs)
+        #     probabilities1.append(np.log(probabilities))
+        # print(probabilities1)
+        # probNum1 = countSpam/self.numTest
 
         #finalprob1.append(np.argmax(sum(probabiltiies1, probNum1)))
         #print("Final probabilities of class1:", finalprob1)
@@ -150,24 +125,30 @@ class spamClassifer():
 #Used Xi, then mean and st. deviation of all x's with that classification
 def calculateProb(data, me, sD):
     # pi = math.pi
-    ex = exp(-((data- me)**2/(2*sD**2)))
-    return (1/ (sqrt(2* pi)* sD))* ex
+    product =1
+    for j in range(0,57):
+        #product = product * (1/sqrt(2*pi*sD[j]))* exp(-0.5*pow((data[j]-me[j]),2)/sD[j])
+        ex = exp(-((data[j]- me[j])**2/(2*sD[j]**2)))
+        product = (1/ (sqrt(2* pi)* sD[j]))* ex
+    return product
 
-#Calculates probability of class for test set
-# def testClassProb (data,cNum ):
-#     countSpam = 0
-#     countNot = 0
-#     for i in data:
-#         if i[57] == 1:
-#             countSpam +=1
-#         else:
-#             countNot +=1
-#     print(countSpam)
-#     if cNum == 1:
-#         print(countSpam/len(data))
-#         return countSpam/len(data)
-#     else:
-#         return countNot/len(data)
+def arrayStatistics(arrays0, arrays1):
+    means0 = []
+    means1 =[]
+    stDevs0 =[]
+    stDevs1 =[]
+    for column in zip(*arrays0):
+        means0.append(mean(column))
+        stDevs0.append(stdev(column))
+    del (means0[-1])
+    del (stDevs0[-1])
+
+    for column in zip(*arrays1):
+        means1.append(mean(column))
+        stDevs1.append(stdev(column))
+    del (means1[-1])
+    del (stDevs1[-1])
+    return means0, stDevs0, means1, stDevs1
 
 #Function to calculate the mean of data passed in
 def mean(data):
